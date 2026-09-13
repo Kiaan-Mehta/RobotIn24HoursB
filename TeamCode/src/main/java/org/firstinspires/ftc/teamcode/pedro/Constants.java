9c04647154488dcf200948cb4eb78b88a20fd7bd
Kiaan Mehta <kiaanmehta@gmail.com>
2026-09-13 07:27:36 -0400

    Completed pedro pathing tuning (#1)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,9 +1,67 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.algorithm.ForesightConfig;
+import com.pedropathing.controllers.Controller;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Matrix;
+import com.pedropathing.math.Vector2D;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
+import com.pedropathing.revhub.localizers.PinpointConfig;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class Constants {
+
+    public static MecanumConfig drivetrainConfig = new MecanumConfig(c -> {
+        c.frontLeftName.set("lf");
+        c.frontRightName.set("rf");
+        c.backLeftName.set("lr");
+        c.backRightName.set("rr");
+        c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+        c.frontRightDirection.set(DcMotorSimple.Direction.REVERSE);
+        c.backLeftDirection.set(DcMotorSimple.Direction.FORWARD);
+        c.backRightDirection.set(DcMotorSimple.Direction.REVERSE);
+    }
+    );
+
+    public static PinpointConfig localizerConfig = new PinpointConfig(c -> {
+        c.name.set("Pinpoint");
+        c.podType.set(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        c.xPodOffset.set(2.25);
+        c.yPodOffset.set(0.0);
+        c.xPodDirection.set(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        c.yPodDirection.set(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        c.globalDistanceUnit.set(DistanceUnit.INCH);
+        c.offsetUnits.set(DistanceUnit.INCH);
+    });
+    public static ForesightConfig foresightConfig = new ForesightConfig(
+            c -> {
+                Controller primaryTranslationalForward = Controller.proportional(0.5226408959398461);
+                Controller secondaryTranslationalForward = Controller.proportional(0.19310184582783244);
+                Controller primaryTranslationalLateral = Controller.proportional(0.38390574021079255);
+                Controller secondaryTranslationalLateral = Controller.proportional(0.14184291285758238);
+
+                c.forwardTranslational.set(Controller.piecewise(secondaryTranslationalForward).put(2.5, primaryTranslationalForward));
+                c.strafeTranslational.set(Controller.piecewise(secondaryTranslationalLateral).put(2.5, primaryTranslationalLateral));
+
+                c.coast.set(Controller.proportionalFeedforward(0.012048373813889804));
+                c.brake.set(Controller.proportionalFeedforward(0.010241117741806333));
+
+                c.headingFeedback.set(Controller.proportional(5.607870167447298));
+                c.headingBrakeCoefficients.set(Vector2D.cartesian(0.07139114605041422, 0.002863465187326952));
+
+                c.linearBrakeCoefficients.set(Matrix.diag(0.07038444742196397, 0.7413593716904866));
+                c.quadraticBrakeCoefficients.set(Matrix.diag(0.0011846288393387108, -0.009360194221473238));
+
+                c.maxAchievableForwardVelocity.set(86.15089421205285);
+                c.maxAchievableStrafeVelocity.set(61.104889500632886);
+                c.naturalForwardDeceleration.set(40.01242239577632);
+                c.naturalStrafeDeceleration.set(69.00106369252273);
+            }
+    );
     public static Follower create(HardwareMap h) {
         // return new Follower(Drivetrain, Localizer, Foresight);
         return null;
